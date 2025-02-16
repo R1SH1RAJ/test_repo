411626d57e2c2b048ced6d0be1fa5f59d9888529
R1SH1RAJ <91680660+R1SH1RAJ@users.noreply.github.com>
2025-02-16 12:03:56 +0530

    First_git_code

--- mainfile.py
+++ mainfile.py
@@ -1,3 +1,4 @@
 def exp(a):
-name = input("Enter your name :")
-print(f"Mr/Mrs your name is : {name}, you are {a} funny!")
+ name = input("Enter your name :")
+ print(f"Mr/Mrs your name is : {name}, you are {a} funny!")
+exp(98)
